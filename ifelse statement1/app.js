a1d27c396b35baea3f14b0b5625fe300b39f9c98
sumitbhskr <sumitbhaskar430@gmail.com>
2025-11-06 17:12:52 +0530

    first commit

---
+++ ifelse statement1/app.js
@@ -0,0 +1,68 @@
+// if else statement =>
+
+// let color = "red";
+
+// if (color === 'red') {
+//     console.log("Stop! light is red");
+// }
+// else if (color === 'yellow') {
+//     console.log("Slow down, light is yellow");
+// }
+// else if (color === 'green') {
+//     console.log("Go, light is yellow");
+// }
+
+
+let marks = 24;
+
+if (marks >= 80) {
+    console.log("A+");
+}
+else if (marks >= 60) {
+    console.log("A");
+}
+else if (marks >= 330) {
+    console.log("B");
+}
+else if (marks <= 33) {
+    console.log("F");
+}
+
+
+let month = "january";
+
+if (month === "january") {
+    console.log("winter is here");
+}
+else if (month === "april") {
+    console.log("summer is here");
+}
+
+// else Statement =>
+
+let age = 18;
+if (age >= 18) {
+    console.log("you can vote");
+}
+else {
+    console.log("you canont vote");
+}
+
+
+let color = "blue";
+
+if (color === "red") {
+    console.log("stop");
+}
+else if (color === "yellow") {
+    console.log("slow down");
+}
+else if (color === "green"){
+    console.log("go");
+}
+else {
+    console.log("traffic light is broken");
+}
+
+
+
