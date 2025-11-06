758171a98c873961808211ee071818273a5e4856
sumitbhskr <sumitbhaskar430@gmail.com>
2025-11-06 17:15:03 +0530

    initial commit

---
+++ Practice Qs1/app.js
@@ -0,0 +1,23 @@
+// Qs Create a system to calulate popcorn prices on the size customer asked for :
+
+// if size is 'XL' price is Rs.250
+// if size is 'L' price is Rs.200
+// if size is 'S' price is Rs.50 =>
+
+
+let size = "M";
+
+if (size === "XL") {
+    console.log("price is Rs. 250");
+}
+else if (size === "L") {
+    console.log("price is Rs. 200");
+}
+else if (size === "M") {
+    console.log("price is Rs. 100");
+}
+else {
+    console.log("price is Rs. 50");
+}
+
+
