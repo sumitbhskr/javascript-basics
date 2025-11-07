5afe1e1d8a12fd99c590dacd1a4232d3250bd69d
sumitbhskr <sumitbhaskar430@gmail.com>
2025-11-07 08:54:43 +0530

    initial commit

---
+++ Practice Qs4/app.js
@@ -0,0 +1,30 @@
+// for the Give String :
+
+// let msg = "help!";
+
+let msg = "hello";
+console.log(msg.trim().toUpperCase());
+
+// Trim it & convert it to uppercase.
+
+// Q2 For the String -> let name = "ApnaCollege", predict the output for following:
+
+// name.slice(4, 9)
+// name.indexOf("na")
+// name.replace("Apna", "Our")
+
+// answer =>
+
+// let name = "ApnaCollege";
+// console.log(name.slice(4, 9));
+// console.log(name.indexOf("na"));
+// console.log(name.replace("Apna", "Our"));
+
+
+// Qs3 Separate the "College" part in above string & replace  'I' with 't' in it. 
+
+let name = "ApnaCollege"
+console.log(name.slice(4).replace('l', 't'));
+console.log(name.slice(4).replace('l', 't').replace('l', 't'));
+
+
