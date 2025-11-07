623ebb22e02e2b6f1009065e9b3d8f242e54717f
sumitbhskr <sumitbhaskar430@gmail.com>
2025-11-07 08:55:30 +0530

    initial commit

---
+++ Logical Operators3/app.js
@@ -0,0 +1,51 @@
+// logical Operators to combine expressions =>
+
+// && logical AND       (exp1) && (exp2)
+
+    // true && true = true 
+    // true && false = false
+    // false && true = true
+    // false && false = false
+// AND opertor =>
+
+// let marks = 90;
+
+// if(marks >= 33 && marks >= 80){
+//     console.log("pass");
+//     console.log("A+");
+// } 
+
+// OR opertor=>
+
+// let marks = 90;
+
+// if(marks >= 33 || marks >= 80){
+//     console.log("pass");
+//     console.log("A+");
+// } 
+
+// ! Logical Not Operator =>
+
+    
+// let marks = 90;
+
+// if (!(marks >= 33) ){
+//     console.log("pass");
+//     console.log("A+");
+// } 
+
+// let marks = 90;
+
+// if (!(marks >= 33)) {
+//     console.log("pass");
+//     console.log("A+");
+// }
+
+
+   
+let marks = 70;
+
+if ((marks >= 33 && marks <= 80) || !false){
+    console.log("pass");
+    console.log("A+");
+} 
