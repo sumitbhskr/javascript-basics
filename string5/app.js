1da8abc918e1a18f2df9211541ef1d2f42ae769a
sumitbhskr <sumitbhaskar430@gmail.com>
2025-11-08 10:22:35 +0530

    initial commit

---
+++ string5/app.js
@@ -0,0 +1,40 @@
+// // String are Immutable in JS=>
+//  No changes can be made to stringify.
+// Whenever we do try to make a change, a new string is created and old one remains same 
+
+// let msg = "  hello  ";
+
+// let password = prompt("set your password");
+// let newPass = password.trim();
+// console.log(password);
+
+
+// Strings Methods with Arguments =>
+    //  Argument is a some value that we pass to the method.
+//  Format =>
+    // stringName.method(arg)
+
+// upperCase=>
+
+    // let msg = "   hello   ";
+
+    // let newMsg = msg.trim();
+    // console.log("after trim : ", newMsg);
+    // newMsg = newMsg.toUpperCase();
+    // console.log("after uppercase : ", newMsg);
+            // ya 
+
+
+    // let msg = "   hello   ";
+    // let newMsg = msg.trim().toUpperCase();   // method chaining
+    // console.log(newMsg);
+
+
+
+// slice =>
+
+    // Returns a part of the original string as a new string.   
+     let msg = "apnacollage";
+     console.log(msg.slice(0, 4));
+
+    
