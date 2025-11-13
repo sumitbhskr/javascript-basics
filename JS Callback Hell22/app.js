eda8128632558c35183ae469e9529ef5d795889d
sumitbhskr <sumitbhaskar430@gmail.com>
2025-11-13 07:11:37 +0530

    initial commit

---
+++ JS Callback Hell22/app.js
@@ -0,0 +1,23 @@
+// Callback Hell => Callback s bhachne k lie promises and asyn or await la use kr t h 
+
+h1 = document.querySelector("h1");
+
+function changeColor(color, delay, nextColorChange) {
+    setTimeout(() => {
+        h1.style.color = color;
+        if (nextColorChange) nextColorChange();
+    }, delay);
+}
+
+
+changeColor("red", 1000, () => {
+    changeColor("orange", 1000, () => {
+        changeColor("green", 1000, () => {
+            changeColor("blue", 1000);
+        });
+    });
+});
+
+
+
+
