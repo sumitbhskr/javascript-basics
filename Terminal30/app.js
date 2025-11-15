5dcf7d84612af02ac883a327f1c16b46dd73468e
sumitbhskr <sumitbhaskar430@gmail.com>
2025-11-15 07:36:10 +0530

    initial commit

---
+++ Terminal30/app.js
@@ -0,0 +1,50 @@
+// Terminal=> A text input and output environment.
+
+// Command line => any inertface that is used by entering textual commands (gen. Windows centric)
+
+
+// Basics commands =>
+// ls = list file(show my files) 
+// pwd = print working directory (where am i?)
+// clear = clear screen 
+
+
+
+//Navigation Command => Inside & Outside Directories 
+// cd = changes directory 
+// cd Desktop 
+// cd.. = back button 
+
+//Paths => Absoulte & Relative
+
+// cd Desktop/Delta(relative path)
+
+// cd/Users/DELL/bhaskar(absolute path)
+
+//  root directoty = /
+// ~   =   home directory
+
+// Making Directories =>  mkdir
+
+// Flags => Flags are characters that we pass with commands to modify their behaviour
+
+// manual Commands =>
+    // man ls = give info about ls command  
+    // man mkdir = give info about mkidir command
+
+// with Flags =>
+// ls -l 
+// ls -a 
+// ls -la
+
+
+// Touch Command => used to create files
+
+// touch index.html 
+// touch app.js
+// touch abc.txt
+
+//Deleting Files & folders =>
+    // rm -remove files 
+//rmdir - removes empty folders
+//rm-rf - removes any folders
