5a855887e76960d982b2f4a723c3189e5900331e
sumitbhskr <sumitbhaskar430@gmail.com>
2025-11-15 07:38:32 +0530

    initial commit

---
+++ Git & Github31/app.js
@@ -0,0 +1,17 @@
+// git => tools that to track changes in code 
+
+// track history
+// help to collabrate
+
+//github => Website where we host repositories online
+
+
+
+// git add =>adds new or changed files in your working directory to the Git staging area.
+
+
+//git commit -m "some message" => it is the record of change
+
+//push (git push -u origin main) => upload local repo content to remote repo
+
+
