7b57934615964906b7d5bdbeea71e684cee4bba0
sumitbhskr <sumitbhaskar430@gmail.com>
2025-11-16 08:11:14 +0530

    initial commit

---
+++ Database 35/app.js
@@ -0,0 +1,21 @@
+// Database => it is a collection of data in a format that can be easily accessed.
+
+//why database =>
+// 1 can store large data 
+// 2 features like securty, scalability etc.
+// 3 Easier to insert, update or delete data
+
+
+
+// Database: SQL=>v/s NoSQL
+
+// SQL => Relational Database (data stored in tables)
+// eg => MySQL, Oracle, PostgreSQL etc 
+
+// NoSQL => Nn Relational Database (data stored in document/key-val/graph etc)
+// eg => MongoDb, Cassandra, Neo4j etc 
+
+
+// SQL (Structured Query Language)
+
+// SQL is a programming langauge used to interact with relational database
