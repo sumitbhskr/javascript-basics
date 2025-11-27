5636dd5a8928cf5714859c06af426b3ab2a0294f
Sumit Bhaskar <sumitbhaskar430@gmail.com>
2025-11-27 08:40:14 +0530

    initial commit

---
+++ javascript TimersAndIntervals/script.js
@@ -0,0 +1,48 @@
+// setTimeout =>
+
+// setTimeout(function(){
+//     console.log("hello");
+// },1000);
+
+
+
+//setInterval =>
+
+// setInterval(function(){
+//     console.log("hello");
+// },1000);
+
+
+// clearTimeout =>
+
+//   let tm = setTimeout(function(){
+//     console.log("hey");
+//   }, 3000);
+  
+//   clearTimeout(tm);
+
+
+
+// clearInterval =>
+
+//   let tm = setInterval(() => {
+    
+//   }, interval);(function(){
+//     console.log("hey");
+//   }, 3000);
+  
+//   clearInterval(tm);
+
+
+
+///  counter => 
+
+//  let count = 10;
+//  let interval = setInterval(function(){
+//     if (count >= 0){
+//       console.log(count);
+//      count--;
+//     
+//     } 
+//     else clearInterval(interval);
+//  }, 1000);
