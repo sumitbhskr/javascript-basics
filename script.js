38fa721560b2b9de2769d0eb28f28ea05321bb23
Sumit Bhaskar <sumitbhaskar430@gmail.com>
2025-12-06 15:38:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// let person = {
+//     name: "Sumit",
+//     age:"24"
+// }
+// console.log(person)
+// console.log(person.name)
+
+// let selectedColors = ["red", "blue"];
+// selectedColors [2] = "green";
+// console.log(selectedColors);
+
+// const cars = ["saab","volvo","BMW"];
+// console.log(cars);
+
+
+// let str ="sumit";
+// let spelitted = str.split("");
+// console.log(spelitted);
+
+//  let str = "Amit";
+//  let spelitted =str.split("");
+//  console.log(spelitted);
+
+// let str = "I am Sumit";
+// let spelitted =str.split("");
+// console.log(spelitted);
+
+//  let str = "I am SUMIT";
+//  let reversedArray = str.split("").reverse();
+// // let finalStr = reversedArray.join(); //comma nhi h
+
+// let finalStr = reversedArray.join(""); //("") h
+// console.log(finalStr);
+ 
+const cars =["Voleo", "BMW", "Saab"];
+cars[0]="Opel";
+console.log(cars);
+
+
+// function reverseString(str) {
+//     let reversed = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i];
+//     }
+//     return reversed;
+// }
+
+// console.log(reverseString("I am SUMIT"));  // TIMUS ma I
+
+let = alert("this pragarph");
+console.log(alert);
